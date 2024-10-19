2adda187d4cc27fc170adde43b5a44bb3d9ab8e7
jezrelljolampong <jezrelljolampong17@gmail.com>
2024-10-19 01:43:48 -0600

    updates

--- server/app.js
+++ server/app.js
@@ -18,9 +18,9 @@ connectDB();
 
 serverRoutes(app);
 
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
 
 app.use(globalErrorHandler);
 
